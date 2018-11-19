86483149dbab968f70f126437ccd5506639db2a0
Usuario <Usuario@SANTHIAGO>
2018-11-19 12:32:28 -0500

    PRUEBA1

---
+++ src/modelo/SelectOperation.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ * @Instituto Rumiñahui
+ * @author COLUMBA SANTIAGO 
+ * SEXTO SEMESTRE
+ */
+public interface SelectOperation {
+    //public void selectOperation();
+    public char jobRobot();
+    public char jobCMC();
+    public char josInspection(); 
+}
